82e4f2951381bb4845be340a39476e011c7a3afd
Suna24 <bastien.t2002@gmail.com>
2023-04-28 18:26:32 +0200

    #1 Creates the user form

--- TextFileToUI/TextFileToUI/UserInfoForm.cs
+++ TextFileToUI/TextFileToUI/UserInfoForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TextFileToUI
 {
-    public partial class Form1 : Form
+    public partial class UserInfoForm : Form
     {
-        public Form1()
+        public UserInfoForm()
         {
             InitializeComponent();
         }
