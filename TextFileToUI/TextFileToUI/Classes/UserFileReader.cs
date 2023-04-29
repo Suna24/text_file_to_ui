00e0e67699fc73e8a56cb93c8b473f55b5495729
Suna24 <bastien.t2002@gmail.com>
2023-04-29 17:00:34 +0200

    #3 Increase the file accessed times attribute when reading a file

--- TextFileToUI/TextFileToUI/Classes/UserFileReader.cs
+++ TextFileToUI/TextFileToUI/Classes/UserFileReader.cs
@@ -14,26 +14,48 @@ namespace TextFileToUI
             FilePath = filePath;
         }
 
-        public void IncreaseNumberOfAccess()
+        /// <summary>
+        /// This method is used to increase the number of access of the file by writing the new dictionnary in the file
+        /// </summary>
+        public void IncreaseNumberOfAccess(Dictionary<string, string> entireDict)
         {
-            /* Not implemented yet */
-        }
-
-        public string ReadFile()
-        {
-
-            var dict = new Dictionary<string, string>();
-
-            foreach (var line in File.ReadAllLines(FilePath))
+            using (StreamWriter writer = new StreamWriter(FilePath))
             {
-                var keyValue = line.Split('=');
-                if (keyValue[0] != "File accessed times")
+                foreach (KeyValuePair<string, string> keyValuePair in entireDict)
                 {
-                    dict.Add(keyValue[0], keyValue[1]);
+                    writer.WriteLine($"{keyValuePair.Key}={keyValuePair.Value}");
                 }
             }
-                
-            return JsonConvert.SerializeObject(dict);
+        }
+
+        /// <summary>
+        /// This method is used to read the file and return the json string of the file
+        /// </summary>
+        public string ReadFile()
+        {
+            
+            var dictUser = new Dictionary<string, string>();
+            var lines = File.ReadAllLines(FilePath);
+
+            /* We go through all lines and add the key value pair in the dictionnary */
+            for(int i = 0; i < lines.Length - 1; i++)
+            {
+                var keyValue = lines[i].Split('=');
+                dictUser.Add(keyValue[0], keyValue[1]);
+            }
+
+            /* We serialize the dictionnary to a json string that will be return at the end */
+            string jsonToReturn = JsonConvert.SerializeObject(dictUser);
+
+            /* We add the last key value pair in the dictionnary (File accessed times attribute) */
+            var lastKeyValuePair = lines[lines.Length - 1].Split('=');
+            dictUser.Add(lastKeyValuePair[0], lastKeyValuePair[1]);
+
+            /* We increase this value by 1 */
+            dictUser["File accessed times"] = (int.Parse(dictUser["File accessed times"]) + 1).ToString();
+            IncreaseNumberOfAccess(dictUser);
+
+            return jsonToReturn;
         }
     }
 }
