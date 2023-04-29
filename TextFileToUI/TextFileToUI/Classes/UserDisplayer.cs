3adb5a2a1cc42e2f792293631fb88ee299bfa6ce
Suna24 <bastien.t2002@gmail.com>
2023-04-29 16:19:51 +0200

    #4 Display data from hard-coded file path

---
+++ TextFileToUI/TextFileToUI/Classes/UserDisplayer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextFileToUI.Classes
+{
+    public class UserDisplayer
+    {
+        public UserInfoForm UserInfoForm { get; set; }
+        public User User { get; set; }
+
+        public UserDisplayer(UserInfoForm userInfoForm, User user)
+        {
+            UserInfoForm = userInfoForm;
+            User = user;
+        }
+
+        public void DisplayUser()
+        {
+            if (User == null)
+            {
+                Console.WriteLine("There is no such user to display");
+            }
+
+            if (UserInfoForm != null)
+            {
+                UserInfoForm.Controls["FullNameTextBox"].Text = User.FullName;
+                UserInfoForm.Controls["YearOfBirthTextBox"].Text = User.YearOfBirth.ToString();
+                UserInfoForm.Controls["CityTextBox"].Text = User.CityOfOrigin;
+                UserInfoForm.Controls["FacultyTextBox"].Text = User.Faculty;
+                UserInfoForm.Controls["RoleTextBox"].Text = User.Role.Name;
+                UserInfoForm.Controls["RoleSpecificAttributeTextBox"].Text = User.Role.SpecificAttribute;
+            } else
+            {
+                Console.WriteLine("There is no form to display the user");
+            }
+        }
+    }
+}
--- TextFileToUI/TextFileToUI/Program.cs
+++ TextFileToUI/TextFileToUI/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TextFileToUI.Classes;
 
 namespace TextFileToUI
 {
@@ -16,7 +17,16 @@ namespace TextFileToUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new UserInfoForm());
+            UserInfoForm userInfoForm = new UserInfoForm();
+
+            UserFileReader userFileReader = new UserFileReader("C:\\Users\\basti\\Documents\\ESIEA\\3A_Semestre_2_CROATIE\\Software_Engineering\\SE - Task with printing data from a text file\\user1.txt");
+            UserParser userParser = new UserParser(userFileReader.ReadFile());
+            User user = userParser.ParseData();
+            UserDisplayer userDisplayer = new UserDisplayer(userInfoForm, user);
+            userDisplayer.DisplayUser();
+
+            Application.Run(userInfoForm);
+
         }
     }
 }
